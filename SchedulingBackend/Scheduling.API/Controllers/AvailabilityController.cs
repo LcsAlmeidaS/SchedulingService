761b6ae5006be4fa04f620ad6a81d0a61a379972
Lucas Almeida <lcshenriquegs@gmail.com>
2026-05-12 09:40:46 -0300

    feat: implement AvailabilityController with GET available slots endpoint

---
+++ SchedulingBackend/Scheduling.API/Controllers/AvailabilityController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Scheduling.API.DTOs.Availability;
+using Scheduling.API.Services.Interfaces;
+
+namespace Scheduling.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AvailabilityController : ControllerBase
+{
+    private readonly IAvailabilityService _availabilityService;
+
+    public AvailabilityController(IAvailabilityService availabilityService)
+    {
+        _availabilityService = availabilityService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAvailableSlots([FromQuery] AvailabilityQueryDto dto)
+    {
+        var result = await _availabilityService.GetAvailableSlotsAsync(dto);
+        return Ok(result);
+    }
+
+}
